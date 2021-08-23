3d9a22f42881cbf2609f28702446e0e05375d7ee
nsohit <52670389+nsohit@users.noreply.github.com>
2021-08-23 20:01:52 +0700

    Add files via upload

---
+++ tes.py
@@ -0,0 +1,22 @@
+a = 1
+b = 1
+c = "Frondend"
+d = 4
+c = "Backend"
+d = 7
+e = 8
+f = "BacKend"
+g = 11
+h = 13
+i = "Frondend"
+print(a)
+while True:
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    print(f)
+    print(g)
+    print(h)
+    print(i)
+    
